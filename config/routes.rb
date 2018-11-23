fd54fcd8b6dbb0b509e2a9f4054dbcbc3d806bee
TAKExHERO <yamaguchi.takehiro0129@gmail.com>
2018-11-24 01:25:27 +0900

    third

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   root 'inquiries#new'
   post 'inquiries/confirm'
   post 'inquiries/thanks'
-  get 'inquiries/thanks'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
