7a260f180d593c37a929d740e788e2455adb45be
TAKExHERO <yamaguchi.takehiro0129@gmail.com>
2018-11-24 01:05:58 +0900

    second-commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            ENV ['LOGIN_NAME'],
+    user_name:            ENV['LOGIN_NAME'],
     password:             ENV['LOGIN_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
