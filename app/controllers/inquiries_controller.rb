cb4ba6c28a255487581beb0c5f2664f9ea460963
TAKExHERO <yamaguchi.takehiro0129@gmail.com>
2018-11-24 10:47:28 +0900

    inquiry-controler

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -13,8 +13,8 @@ class InquiriesController < ApplicationController
   end
 
   def thanks
-     @inquiry = Inquiry.new(inquiry_params)
-      InquiryMailer.received_email(@inquiry).deliver_now
+    @inquiry = Inquiry.new(inquiry_params)
+    InquiryMailer.received_email(@inquiry).deliver_now
   end
 
   private
@@ -22,5 +22,4 @@ class InquiriesController < ApplicationController
   def inquiry_params
     params.require(:inquiry).permit(:name, :email, :relationship, :content)
   end
-
 end
