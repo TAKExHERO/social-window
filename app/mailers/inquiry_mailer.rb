5bf0e54aa4d30200be6d1ea3bdeb8674c01db779
TAKExHERO <yamaguchi.takehiro0129@gmail.com>
2018-11-24 11:34:45 +0900

    inquiry-mailer

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,7 +1,6 @@
 class InquiryMailer < ApplicationMailer
   def received_email(user)
     @user = user
-    mail to: user.email,
-         subject: "【#{@user.name}様の社会の窓についてお知らせがあります】"
+    mail(to: user.email, subject: "【#{@user.name}様の社会の窓についてお知らせがあります】")
   end
 end
